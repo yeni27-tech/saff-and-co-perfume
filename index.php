351a61d0b913b148ddcd0829df0bb758dbc0ded6
yeni27-tech <yenifaturohmah2@gmail.com>
2024-01-24 23:37:08 +0700

    saff & co.

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=1, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="shortcut icon" href="#">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <title>Saff & Co.</title>
+</head>
+<body>
+    <navbar>
+        <div class="logo">
+            <img src="./img/logo-white.webp" alt="">
+        </div>
+        <div class="nav"></div>
+    </navbar>
+</body>
+</html>
